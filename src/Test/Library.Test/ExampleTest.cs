9a3f21dd319be25c2be37cd13a4bdcb6a06fabb2
GuillermoRocca <110860311+GuillermoRocca@users.noreply.github.com>
2022-09-10 18:55:11 -0300

    Initial commit

---
+++ src/Test/Library.Test/ExampleTest.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+
+namespace Test.Library
+{
+    public class ExampleTest
+    {
+        [Test]
+        public void dummyTest()
+        {
+            Assert.True(true);
+        }
+    }
+}
